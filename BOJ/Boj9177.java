6e3b869bff321c062eedc513ba644bfc35b0362a
jaehyun <keejj9292@naver.com>
2022-02-27 17:02:37 +0900

    단어섞기

---
+++ BOJ/Boj9177.java
@@ -0,0 +1,51 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj9177 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		int input = Integer.parseInt(in.readLine());
+
+		int dp[] = new int[input + 1];
+
+		for (int i = 1; i <= input; i++) {
+			int val = 1;
+
+			while (true) {
+				int result = (int) Math.pow(val, 2);
+
+				if (result == i) {
+					dp[i] = 1;
+					break;
+				}
+
+				else if (result < i) {
+					if (dp[i] == 0) {
+						dp[i] = 1 + dp[i - result];
+					} else {
+						
+
+						
+						dp[i] = Math.min(dp[i], 1 + dp[i - result]);
+					}
+				}
+
+				else {
+					break;
+				}
+
+				val++;
+			}
+
+		}
+		
+		System.out.println(dp[input]);
+
+	}
+}
