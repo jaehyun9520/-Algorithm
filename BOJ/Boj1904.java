3018e9235f143ac0cf43499dee98403f22bc997b
jaehyun <keejj9292@naver.com>
2022-07-10 21:55:25 +0900

    01타일

---
+++ BOJ/Boj1904.java
@@ -0,0 +1,26 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj1904_01타일 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(in.readLine());
+
+		int dp[] = new int[n + 1];
+		dp[1] = 1;
+
+		if(n>=2) dp[2]=2;
+		
+		for (int i = 3; i <= n; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+		}
+
+		System.out.println(dp[n]);
+	}
+}
