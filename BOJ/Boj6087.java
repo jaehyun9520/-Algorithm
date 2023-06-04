d5ac358e69a925314e88fff552913905dcc04eea
jaehyun <keejj9292@naver.com>
2023-06-04 15:26:58 +0900

    레이저 통신

--- BOJ/Boj6087.java
+++ BOJ/Boj6087.java
@@ -1,4 +1,4 @@
-package boj;
+package 백준;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,169 +6,111 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Boj6087_레이저통신 {
+public class Boj6087 {
 
 	static class State implements Comparable<State> {
-		
-		int x, y,  dir, cost;
-		
-		State(int x, int y, int dir, int cost)
-		{
-			this.x=x;
-			this.y=y;
-			this.dir=dir;
-			this.cost=cost;
+		int x, y, dir, cnt;
+
+		State(int x, int y, int dir, int cnt) {
+			this.x = x;
+			this.y = y;
+			this.dir = dir;
+			this.cnt = cnt;
 		}
 
+		// 거울 사용횟수 오름차순으로 정렬
 		@Override
-		public int compareTo(State o) {  //오름차순 정렬 (작은게 먼저 나오게 )
-			return this.cost-o.cost;
-		}
+		public int compareTo(State o) {
 
+			return this.cnt - o.cnt;
+
+		}
 	}
-	
-	
-			// 상 우 하 좌 
-	static int dx[]= {-1,0,1,0};
-	static int dy[]= {0,1,0,-1};
-	static int dc[]= {0,1,-1};
-	static int map[][];
-	static int W,H;
-	static int ans=0;
-	static int sx=0, sy=0;
-	static int cost[][];
+
+	static int[] dx = { -1, 0, 1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
+	static int w, h;
+	static int[][] map;
+	static boolean[][][] visited;
+	static int[] mirror = { -1, 1 };
+	static int sx, sy, ans = 0;
+
 	public static void main(String[] args) throws IOException {
-		
-		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(in.readLine()," ");
-		
-		
-		W=Integer.parseInt(st.nextToken());
-		H=Integer.parseInt(st.nextToken());
-		
-		cost=new int[H+1][W+1];
-		map=new int[H+1][W+1];
-		boolean flag=false;
-		for(int i=1; i<=H; i++)
-		{
-			
-			String input=in.readLine();
-			
-			
-			for(int j=1; j<=W; j++)
-			{
 
-				cost[i][j]=-1;
-				map[i][j]=input.charAt(j-1);
-				
-				if(!flag&&map[i][j]=='C')
-				{
-					flag=true;
-					sx=i; sy=j;
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+
+		w = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+
+		map = new int[h + 1][w + 1];
+		visited = new boolean[4][h + 1][w + 1];
+
+		for (int i = 1; i <= h; i++) {
+
+			String input = in.readLine();
+
+			for (int j = 1; j <= w; j++) {
+
+				map[i][j] = input.charAt(j - 1);
+
+				if (map[i][j] == 'C') {
+					sx = i;
+					sy = j;
 				}
+
 			}
 		}
-		
-	
-		
-		dijkstra();
-		
+
+		bfs();
+
 		System.out.println(ans);
-		
-		
-		
 	}
-	
-	
-	static void dijkstra()
-	{
-		cost[sx][sy]=0;
-	
-	
-		PriorityQueue<State> PQ=new PriorityQueue<>();		
-		
-		for(int i=0; i<=3; i++)
-		{
-			int nx=sx+dx[i];
-			int ny=sy+dy[i];
-			
-			if(nx<1||nx>H||ny<1||ny>W||map[nx][ny]=='*') continue; 
-			
-			
-			cost[nx][ny]=0;
-			PQ.add(new State(nx,ny,i,0));
+
+	static void bfs() {
+
+		PriorityQueue<State> pq = new PriorityQueue<>();
+
+		for (int i = 0; i <= 3; i++) {
+
+			visited[i][sx][sy] = true;
+			pq.add(new State(sx, sy, i, 0));
 		}
-		
-		
-		while(!PQ.isEmpty())
-		{
-			State now=PQ.poll();
-			
-			if(cost[now.x][now.y]==-1||cost[now.x][now.y]==now.cost)
-			{
-				cost[now.x][now.y]=now.cost;
-				
-				if(!(now.x==sx&&now.y==sy)&&map[now.x][now.y]=='C')
-				{
-					
-					ans=now.cost;
-					break;
-				}
-				int dir=now.dir;
-				for(int i=0; i<=2; i++)
-				{
-					
-					int next=dir+dc[i];
-					
-					if(next==-1) next=3;
-					else if(next==4) next=0;
-					
-					int nx=now.x+dx[next];
-					int ny=now.y+dy[next];
-					
-					//막혀있거나 맵을 벗어나는 경우 생략 
-					if(nx<1||nx>H||ny<1||ny>W||map[nx][ny]=='*') continue;
-					
-					
-					if(i!=0&&(cost[nx][ny]==-1||cost[nx][ny]==now.cost+1))
-					{
-						
-						
-						
-						PQ.add(new State(nx,ny,next,now.cost+1));
-					}
-					
-					else if(i==0&&(cost[nx][ny]==-1||cost[nx][ny]==now.cost)){
-						
-					
-					
-						PQ.add(new State(nx,ny,dir,now.cost));
-					}
-					
-					
-					
-					
-				}
-				
-				
-				
+
+		while (!pq.isEmpty()) {
+
+			State now = pq.poll();
+
+			int nx = now.x + dx[now.dir];
+			int ny = now.y + dy[now.dir];
+
+			if (nx < 1 || nx > h || ny < 1 || ny > w || visited[now.dir][nx][ny] || map[nx][ny] == '*')
+				continue;
+
+			visited[now.dir][nx][ny] = true;
+
+			if (map[nx][ny] == 'C') {
+				ans = now.cnt;
+				return;
 			}
-			
-			
-			
-			
-			
-			
-			
-			
+
+			pq.add(new State(nx, ny, now.dir, now.cnt));
+
+			for (int i = 0; i <= 1; i++) {
+
+				int newDir = now.dir + mirror[i];
+
+				if (newDir == 4)
+					newDir = 0;
+				if (newDir == -1)
+					newDir = 3;
+
+				pq.add(new State(nx, ny, newDir, now.cnt + 1));
+
+			}
+
 		}
-		
-		
-		
-		
-		
-		
+
 	}
-	
-	
+
 }
