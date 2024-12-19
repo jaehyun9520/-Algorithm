95e5b78762d956c7c6b78b42e067036b77c1e023
jaehyun9520 <64720923+jaehyun9520@users.noreply.github.com>
2024-12-20 08:43:41 +0900

    지름길 완전탐색 풀이추가

--- BOJ/Boj1446.java
+++ BOJ/Boj1446.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
 
 public class Boj1446 {
 
-
+// 풀이 방법 1. dp
   public static void main(String[] args) throws Exception {
 
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -64,3 +64,72 @@ public class Boj1446 {
 
   }
 }
+
+
+
+// 풀이방법 2 완전탐색
+
+public class Boj1446 {
+
+  static int highwayLength;
+  static Map<Integer, List<int[]>> shortcutMap;
+  static int ans = Integer.MAX_VALUE;
+
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+
+    // 지름길 개수와 고속도로 길이
+    int cnt = Integer.parseInt(st.nextToken());
+    highwayLength = Integer.parseInt(st.nextToken());
+
+    shortcutMap = new HashMap<>();
+
+    for (int i = 1; i <= cnt; i++) {
+
+      st = new StringTokenizer(in.readLine(), " ");
+
+      int start = Integer.parseInt(st.nextToken());
+      int end = Integer.parseInt(st.nextToken());
+      int length = Integer.parseInt(st.nextToken());
+
+      if (!shortcutMap.containsKey(start)) {
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{end, length});
+        shortcutMap.put(start, list);
+      } else {
+        shortcutMap.get(start).add(new int[]{end, length});
+      }
+    }
+    // 완전탐색 시작 ( 현재 고속도로 위치, 목표 고속도로 위치, 실제 운전해야 하는 거리)
+    dfs(0, 0);
+
+    System.out.println(ans);
+  }
+
+  static void dfs(int loc, int len) {
+
+    if (len > highwayLength) {
+      return;
+    }
+
+    if (loc == highwayLength) {
+      ans = Integer.min(len, ans);
+      return;
+    }
+
+    dfs(loc + 1, len + 1);
+
+    // 현재 위치에서 갈 수 있는 지름길이 존재한다면?
+    if (shortcutMap.containsKey(loc)) {
+
+      List<int[]> list = shortcutMap.get(loc);
+
+      for (int[] info : list) {
+        dfs(info[0], len + info[1]);
+      }
+    }
+  }
+}
+
